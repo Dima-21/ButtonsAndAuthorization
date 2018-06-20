d4e6d4cf2b52f03a240bbe0d0baa1bda21873fc4
student <student@K09-10>
2018-06-20 17:01:26 +0300

    1.0

---
+++ 02-Button/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _02_Button
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int i = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RepeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            text1.Text += $"{i++} ";
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            var checks = Parent1.Children;
+            foreach (var elem in checks)
+            {
+                var cb = (CheckBox)elem as CheckBox;
+                if(cb!=null)
+                {
+                    string s = $"{cb.Content.ToString()} => {cb.IsChecked}";
+                    MessageBox.Show(s);
+                }
+              
+            }
+        }
+    }
+}
---
+++ 03-Login/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security.Cryptography;
+
+namespace _03_Login
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        static string GetMd5Hash(string input)
+        {
+            MD5 md5Hash = MD5.Create();
+            
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+
+        private void submit_Click(object sender, RoutedEventArgs e)
+        {
+            string Login = login.Text;
+            string Password = password.Password;
+            Password = GetMd5Hash(Password);
+            MessageBox.Show($"{Login} -> {Password}");
+        }
+    }
+}
